42fe878b7fb75223259d8c035530df66ff119c65
br0nc080 <mwelbu2@gmail.com>
2011-12-09 23:58:46 -0600

    Initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ranker::Application.config.secret_token = '7dfbf5f5d898a91e1a24bac1f8a9c89f9bb99c86981c6de2bdd6365d98a0b62fb18832159996bc64d359ebacc17b4bfa9d6d7428896e9e4dd37f99dcaf710354'
